62d834cd1003483692d11f90ebb2e33fc02a8629
YumenoFR <y.seki.funrepeat@gmail.com>
2024-09-13 19:11:57 +0900

    Update authMiddleware.ts

--- src/app/api/authMiddleware.ts
+++ src/app/api/authMiddleware.ts
@@ -1,22 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { supabase } from '@/lib/supabaseClient';
 
+// authMiddleware.ts
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get('sb:token');
-  
+  const token = req.cookies.get('token')?.value; // RequestCookieからvalueを取得
+
   if (!token) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  const { data: user } = await supabase.auth.getUser(token);
+  // tokenがstringであることを確認
+  const { data: user, error } = await supabase.auth.getUser(token as string);
 
-  if (!user) {
+  if (error || !user) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ['/protected/*'], // 認証が必要なページパスを指定
-};
